052fe791cf2319eb487dc51db716c56cbd3c43d5
unknown <davidluase17@gmail.com>
2025-12-17 00:03:02 +0700

    Initial commit

---
+++ backend/src/article/article.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ArticleService } from './article.service';
+import { createArticleDto } from './dto/create-article.dto';
+import type { IArticle } from './interface/article.interface';
+
+@Controller('article')
+export class ArticleController {
+  constructor(private readonly articleService: ArticleService) {}
+
+  @Get()
+  findAll(): IArticle[] {
+    return this.articleService.findAllArticle();
+  }
+
+  @Get('/:id')
+  findOne(@Param() params: any): string {
+    return `Tampil detail Article ${params.id}`;
+  }
+
+  @Post()
+  create(@Body() createArticleDto: createArticleDto): IArticle {
+    return this.articleService.createArticle(createArticleDto);
+  }
+
+  @Put('/:id')
+  update(@Param() params: any): string {
+    return `Update Article ${params.id}`;
+  }
+
+  @Delete('/:id')
+  delete(@Param() params: any): string {
+    return `Delete Article ${params.id}`;
+  }
+}
---
+++ backend/src/article/dto/create-article.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsString, IsEnum } from 'class-validator';
+import { ArticleStatus } from '../interface/article.interface';
+
+export class createArticleDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsNotEmpty()
+  @IsEnum(ArticleStatus)
+  status: ArticleStatus;
+}
