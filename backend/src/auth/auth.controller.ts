36d37c9f94651ec83d199c5450e13af7c5d50ba6
unknown <davidluase17@gmail.com>
2026-01-19 16:01:40 +0700

    add role guard

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Controller,
   Get,
+  HttpCode,
+  HttpStatus,
   Post,
   Request,
   UseGuards,
@@ -11,6 +13,9 @@ import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { User } from './entities/user.entity';
 import { AuthGuard } from './guard/auth.guard';
+import { RolesGuard } from './guard/role.guard';
+import { Role } from './enum/role.enum';
+import { Roles } from './decorator/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -21,6 +26,7 @@ export class AuthController {
     return await this.authService.registerUser(registerDto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.loginUser(loginDto);
@@ -30,5 +36,12 @@ export class AuthController {
   @UseGuards(AuthGuard)
   async getUser(@Request() request): Promise<User | null> {
     return await this.authService.getUser(request.user.id);
+  }
+
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get('test')
+  getTest(): { message: string } {
+    return { message: 'Test role guard berhasil' };
   }
 }
---
+++ backend/src/auth/guard/role.guard.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../enum/role.enum';
+import { ROLES_KEY } from '../decorator/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Access denied');
+    }
+
+    return true;
+  }
+}
